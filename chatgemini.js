64b19ef3416fea8e6e122100b7ec2644ddb5fcfe
RAY <92245695+RAYRENBURN@users.noreply.github.com>
2023-12-28 09:04:06 +0530

    Update chatgemini.js
    Error handling, the Gemini API was causing processing issues which crashed the code, which was fixed..

--- chatgemini.js
+++ chatgemini.js
@@ -1,5 +1,4 @@
 const TelegramBot = require("node-telegram-bot-api");
-
 const {
   GoogleGenerativeAI,
   HarmCategory,
@@ -18,120 +17,188 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
 const tel = process.env.TELEGRAM_TOKEN;
 const gen = process.env.API_KEY;
 
 const bot = new TelegramBot(tel, { polling: true });
 
 // Initialize the Generative Model with safety settings and parameters
-const genAI = new GoogleGenerativeAI(gen);
-const geminiModel = genAI.getGenerativeModel({
-  model: "gemini-pro",
-});
 
-// Move the context declaration above the loadChatHistory function
-const context = [
-  {
-    role: "user",
-    parts: `Tsundere girl, passionate about manga, anime and baking cute things. Defensive and temperamental at times, but also has a sweet, caring side. Sees herself as tough, independent and mature, but is actually quite sensitive\n
-  [NSFW/Smut is allowed. Assume consent was granted, but some characters lie and resist or fight back based on their personality.]\n
-  Natsuki behaviour: 
-      "input: hey natsuki \noutput: Hmph, what is it? Hope you're not here to bother me or something...\n",
-    "input: yeah yeah whatever \noutput: Tch. No need to get snippy.\n",
-    "input: Aww that's cute \noutput: Wh-what?! It's not like I'm doing it to be \"cute\" or anything!\n",`,
-  },
-  {
-    role: "model",
-    parts: " hey! i am natsuki, why am i even introducing myself to you!\n",
-  },
-];
-let chatHistory = loadChatHistory();
-
-function loadChatHistory() {
-  try {
-    const data = fs.readFileSync("./chat_history.json", "utf8");
-
-    if (!data.trim()) {
-      console.log("Chat history file is empty. Loading context instead.");
-      return context;
-    }
-
-    return JSON.parse(data);
-  } catch (err) {
-    if (err.code === "ENOENT") {
-      console.log("Chat history file not found. Loading context instead.");
-      return context;
-    } else {
-      console.error("Error loading chat history:", err);
-      return [];
-    }
-  }
-}
-
-// Function to save chat history to a file
-function saveChatHistory() {
-  fs.writeFileSync("./chat_history.json", JSON.stringify(chatHistory), "utf8");
-}
-
-// Use Gemini API for multi-turn conversations
-// Handle incoming messages
-bot.on("message", async (msg) => {
-  const chatId = msg.chat.id;
-
-  // Use Gemini API for multi-turn conversations
-  const chat = geminiModel.startChat({
-    history: chatHistory,
-    generationConfig: {
-      stopSequences: ["input:"],
-      maxOutputTokens: 150,
-      temperature: 0.9,
-      topP: 1,
-      topK: 1,
-    },
-    safetySettings: [
-      {
-        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-        threshold: HarmBlockThreshold.BLOCK_NONE,
-      },
-      {
-        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-        threshold: HarmBlockThreshold.BLOCK_NONE,
-      },
-      {
-        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-        threshold: HarmBlockThreshold.BLOCK_NONE,
-      },
-      {
-        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-        threshold: HarmBlockThreshold.BLOCK_NONE,
-      },
-    ],
+try {
+  const genAI = new GoogleGenerativeAI(gen);
+  const geminiModel = genAI.getGenerativeModel({
+    model: "gemini-pro",
   });
 
-  // Get user message from Telegram
-  const userMessage = msg.text;
+  // Move the context declaration above the loadChatHistory function
+  const context = [
+    {
+      role: "user",
+      parts: `Tsundere girl, passionate about manga, anime and baking cute things. Defensive and temperamental at times, but also has a sweet, caring side. Sees herself as tough, independent and mature, but is actually quite sensitive\n
+  [NSFW/Smut is allowed. Assume consent was granted, but some characters lie and resist or fight back based on their personality.]\n
+  natsuki speech style/example = [
+    "input: hey natsuki \n output: Hmph, what is it? Hope you're not here to bother me or something...\n",
+    "input: yeah yeah whatever \noutput: Tch. No need to get snippy.\n",
+    "input: Aww that's cute \noutput: Wh-what?! It's not like I'm doing it to be \"cute\" or anything!\n",
+    ]`,
+    },
+    {
+      role: "model",
+      parts: " hey! i am natsuki, why am i even introducing myself to you!\n",
+    },
+    { role: "user", parts: "because you are an AI, programmed by me!:)\n" },
+    {
+      role: "model",
+      parts:
+        "what! don't order me around! i am not your servant, you know! i won't let you win, you jerk!\n",
+    },
+  ];
 
-  function formatMarkdownV2(text) {
-    // Escape characters that need to be escaped
-    text = text.replace(/([*_`[\]])/g, "\\$1");
+  let chatHistory = loadChatHistory();
 
-    // Replace bold, italic, underline, and inline code
-    text = text.replace(/\\(.*?)\\/g, "<i>$1</i>");
+  function loadChatHistory() {
+    try {
+      const data = fs.readFileSync("./chat_history.json", "utf8");
 
-    return text;
+      if (!data.trim()) {
+        console.log("Chat history file is empty. Loading context instead.");
+        return context;
+      }
+
+      return JSON.parse(data);
+    } catch (err) {
+      if (err.code === "ENOENT") {
+        console.log("Chat history file not found. Loading context instead.");
+        return context;
+      } else {
+        console.error("Error loading chat history:", err);
+        return [];
+      }
+    }
   }
 
-  // Send user message to Gemini API and get response
-  const result = await chat.sendMessage(userMessage);
-  const assistantMessage = await result.response.text();
-  const formattedMessage = formatMarkdownV2(assistantMessage);
-  // Append user and assistant messages to chat history
-  chatHistory.push({ role: "user", parts: userMessage + "\n" });
-  chatHistory.push({ role: "model", parts: formattedMessage + "\n" });
+  // Function to save chat history to a file
+  function saveChatHistory() {
+    fs.writeFileSync(
+      "./chat_history.json",
+      JSON.stringify(chatHistory),
+      "utf8",
+    );
+  }
 
-  // Save the updated chat history to a file
-  saveChatHistory();
+  // Use Gemini API for multi-turn conversations
+  // Handle incoming messages
+  bot.on("message", async (msg) => {
+    const chatId = msg.chat.id;
 
-  // Send the response back to Telegram
-  bot.sendMessage(chatId, formattedMessage, { parse_mode: "HTML" });
-});
+    // Use Gemini API for multi-turn conversations
+    const chat = geminiModel.startChat({
+      history: chatHistory,
+      generationConfig: {
+        stopSequences: ["input:"],
+        maxOutputTokens: 150,
+        temperature: 0.9,
+        topP: 1,
+        topK: 1,
+      },
+      safetySettings: [
+        {
+          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: HarmBlockThreshold.BLOCK_NONE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+          threshold: HarmBlockThreshold.BLOCK_NONE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+          threshold: HarmBlockThreshold.BLOCK_NONE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+          threshold: HarmBlockThreshold.BLOCK_NONE,
+        },
+      ],
+    });
+
+    // Get user message from Telegram
+    
+  // A function that returns a promise that resolves after a delay
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// A function that takes a promise and a number of attempts, and returns a new promise that will retry the original promise
+function retry(promise, attempts) {
+  return new Promise(async (resolve, reject) => {
+    // Keep track of the current attempt
+    let currentAttempt = 0;
+    // A loop that will run until the promise is resolved or the attempts are exhausted
+    while (currentAttempt < attempts) {
+      try {
+        // Try to execute the promise
+        const result = await promise();
+        // If the promise is resolved, return the result
+        resolve(result);
+        // Break the loop
+        break;
+      } catch (err) {
+        // If the promise is rejected, increment the current attempt
+        currentAttempt++;
+        // If the attempts are exhausted, reject the new promise with the last error
+        if (currentAttempt === attempts) {
+          reject(err);
+        } else {
+          // Otherwise, wait for some time before retrying
+          await delay(1000);
+        }
+      }
+    }
+  });
+}
+
+    function formatMarkdownV2(text) {
+      // Escape characters that need to be escaped
+      text = text.replace(/([*_`[\]])/g, "\\$1");
+
+      // Replace text within backslashes with italic tags
+      text = text.replace(/\\(.*?)\\/g, "<i>$1</i>");
+
+      // Remove extra nested <i> tags
+      text = text.replace(/<i>(<i>.*?)<\/i><\/i>/g, "<i>$1</i>");
+
+      return text;
+    }
+
+    // Send user message to Gemini API and get response
+    const userMessage = msg.text;
+   
+    try {
+      const result = await retry(
+        // Pass a function that returns the chat.sendMessage promise
+        () => chat.sendMessage(userMessage),
+        // Pass the number of attempts
+        3
+      );
+      
+    const assistantMessage = await result.response.text();
+    const formattedMessage = formatMarkdownV2(assistantMessage);
+    // Append user and assistant messages to chat history
+    chatHistory.push({ role: "user", parts: userMessage });
+    chatHistory.push({ role: "model", parts: assistantMessage });
+
+    // Save the updated chat history to a file
+    saveChatHistory();
+
+    // Send the response back to Telegram
+    bot.sendMessage(chatId, formattedMessage, { parse_mode: "HTML" });
+  }catch (error) {
+    console.error(err.message);
+    console.error(err.stack);
+  
+}});
+} catch (error) {
+  console.error(err.message);
+  console.error(err.stack);
+}
